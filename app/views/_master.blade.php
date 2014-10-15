a0dc2da92f28bac089873ed02b61212235b5a10f
Sam Seaborn <pcisnerosfernandez@g.harvard.edu>
2014-10-14 22:07:14 -0500

    some style and landing page

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -7,7 +7,7 @@
     
     <meta name='viewport' content='width=device-width, initial-scale=1'>
 	
-	<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/cerulean/bootstrap.min.css" rel="stylesheet">
+	<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/journal/bootstrap.min.css" rel="stylesheet">
     
    <link rel="stylesheet" href="{{ URL::asset('/') }}css/mystyle.css" />.
 	
@@ -40,24 +40,47 @@
 
 
 <h2 id="text">Lorem Ipsum Generator</h2>
-<blockquote>
+<blockquote >
 	Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. While lorem ipsum's still resembles classical Latin, it actually has no meaning whatsoever.
 </blockquote>
-
-<p>Create random filler text for your applications.</p>
-
-<a href='/lorem-ipsum'>Go to the App!</a>
+	
+	Number of Paragraphs.
+    
+   <hr>
+	
+	<form method='POST'>
+	
+		<input name="_token" type="hidden" value="pmJyVtgim9YsTFaorfA9Jn6HUPIfGz25kF2YuJwx">	
+		<label id=text for="paragraphs">Paragraphs</label>				
+		<input maxlength="2" name="paragraphs" type="text" value="10" id="paragraphs"> Max 10
+		
+		<br><br>
+	<hr>		
+		<input type="submit" value="CREATE">    
+    </form>
 
 
 <br>
 
 <h2 id= "text">Random User Generator</h2>
-<p>A free API for generating random user data.
-    </p>
-<p>Create random user data for your application.
-    </p>
-<a href='/user-generator'>Go to the App!</a>
 
+<p>Random user Data.
+    </p>
+
+		<hr>		
+	<form method="POST" action="PENDIENTE" accept-charset="UTF-8"><input name="_token" type="hidden" value="pmJyVtgim9YsTFaorfA9Jn6HUPIfGz25kF2YuJwx">	
+	
+        <label id=text for="users">How many users?</label>		<input maxlength="2" name="users" type="text" value="10" id="users"> Max 10
+		<br>
+		
+		<br>
+        <input name="telephone" type="checkbox">		<label id=text for="telephone">Telephone</label>		<br>
+        
+		<input name="birthdate" type="checkbox">		<label id=text for="birthdate">Birthdate</label>		<br>
+			<hr>	
+		<input type="submit" value="CREATE">    
+    </form>
+    
 
 	
 </div>
